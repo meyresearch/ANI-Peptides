0c5754f397f42a5999178d587a7bc83e4d6382a7
Matthew Bowley <m.bowley98@gmail.com>
2022-02-22 08:09:38 +0000

    add __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""
+anipep
+
+A library to compare ANI and AMBER simulations of small peptides
+"""
+
+__version__ = "1.0.0"
+__author__ = 'Matthew Bowley <https://github.com/not-matt>'
+__credits__ = 'Mey Research, University of Edinburgh'
